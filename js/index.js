e3ace8a066bf4c18c697cc72bb6f9860f09d0d7d
Liveta Valantiejūtė <l.valantiejute@gmail.com>
2020-08-17 21:22:00 +0300

    added project descriptions

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var acc = document.getElementsByClassName("more-info");
+//var acc = document.getElementsByClassName("project");
+//var acc = project.lastChild;
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.parentElement.parentElement.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
