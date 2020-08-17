20d0114db802f5bb4f8b461967117e67b0fce56b
Liveta Valantiejūtė <l.valantiejute@gmail.com>
2020-08-17 17:41:01 +0300

    fixed buttons

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,26 @@
-//const { src, dest, series } = require("gulp");
+const { src, dest, series } = require("gulp");
 const fileinclude = require("gulp-file-include");
 const sass = require('gulp-sass');
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-gulp.task('fileinclude', function(done) {
-  return gulp.src(['html/index.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./'));
-    done();
-});
-
-// gulp.task('browserSync', function() {
-//   browserSync.init(["css/*.css", "js/*.js", "scss/.scss"],{
-//     server: {
-//       baseDir: './'
-//     },
-//   })
+// gulp.task('fileinclude', function() {
+//   return gulp.src(['html/index.html'])
+//     .pipe(fileinclude({
+//       prefix: '@@',
+//       basepath: '@file'
+//     }))
+//     .pipe(gulp.dest('./'));
 // });
 
-// gulp.task('sass', function() {
-//     return gulp.src('scss/**/*.scss')
-//       .pipe(sass())
-//       .pipe(gulp.dest('css'))
-//       .pipe(browserSync.reload({
-//         stream: true
-//       }))
-//   })
+function include() {
+  return gulp.src('html/index.html')
+  .pipe(fileinclude ({
+    prefix: '@@',
+    basepath: '@file'
+  }))
+  .pipe(gulp.dest('./'));
+}
 
   // gulp.task('watch', function(){
   //   // gulp.watch('scss/**/*.scss', gulp.series(['sass'])).on('change', browserSync.reload);
@@ -40,7 +31,21 @@ function style() {
   return gulp.src('./scss/**/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./html/*.html', include).on('change', browserSync.reload);
+  gulp.watch('./index.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
-exports.fileinclude = fileinclude;
+exports.include = include;
+exports.watch = watch;
